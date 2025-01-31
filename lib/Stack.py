18b32d48790a18131c731836e9e505878f0cb20c
Thomas <thomasrains01@gmail.com>
2025-01-31 12:56:15 -0800

    submits lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
+    def __init__(self, items = [], limit = 100): 
+        
+        self.items = items
+        self.limit = limit
         pass
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+
 
     def push(self, item):
-        pass
+            if len(self.items) < self.limit:
+                self.items.append(item)
+            else:
+                OverflowError("Stack is full")
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None  # Check if the stack is not empty
+              # Remove the last item
+        else: return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+
+         return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        reversed_items = list(reversed(self.items))  # Reverse the stack to treat the top as the first element
+        try:
+            # Find the index of the target in the reversed list
+            return reversed_items.index(target)
+        except ValueError:
+            return -1
+
